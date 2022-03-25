e1467eba08215e17e26f50bbf9d4d549d96a7bd2
mfv1999 <91701007+mfv1999@users.noreply.github.com>
2022-03-25 23:37:13 +0100

    pais y pais dao basicos terminados
    siguiente tabla a implementrar jugadores

--- Tenis/Dominio/Pais.vb
+++ Tenis/Dominio/Pais.vb
@@ -7,4 +7,29 @@
         Me.paisDao = New PaisDAO
     End Sub
 
+    Public Sub New(id As String)
+        Me.paisDao = New PaisDAO
+        Me.idPais = id
+    End Sub
+
+    Public Sub New(id As String, nombre As String)
+        Me.idPais = id
+        Me.nombrePais = nombre
+    End Sub
+
+    Public Sub leerTodosPaises()
+        Me.paisDao.leerTodos()
+    End Sub
+
+    Public Function insertarPais() As Integer
+        Return Me.paisDao.insertar(Me)
+    End Function
+
+    Public Function actualizarPais() As Integer
+        Return Me.paisDao.Actualizar(Me)
+    End Function
+
+    Public Function borrarPais() As Integer
+        Return Me.paisDao.Borrar(Me)
+    End Function
 End Class
--- Tenis/Persistencia/PaisDAO.vb
+++ Tenis/Persistencia/PaisDAO.vb
@@ -16,5 +16,25 @@
         Next
     End Sub
 
+    Public Sub leer(ByRef p As Pais)
+        Dim col As Collection : Dim aux As Collection
+        col = AgenteBD.ObtenerAgente.Leer("SELECT * FROM Paises WHERE idPais='" & p.idPais & "';")
+
+        For Each aux In col
+            p.nombrePais = aux(2).ToString
+        Next
+    End Sub
+
+    Public Function insertar(ByVal p As Pais) As Integer
+        Return AgenteBD.ObtenerAgente.Modificar("INSTERT INTO Paises VALUES ('" & p.idPais & "','" & p.nombrePais & "');")
+    End Function
+
+    Public Function Borrar(ByVal p As Pais) As Integer
+        Return AgenteBD.ObtenerAgente.Modificar("DELETE FROM Paises WHERE IDPersona='" & p.idPais & "';")
+    End Function
+
+    Public Function Actualizar(ByVal p As Pais) As Integer
+        Return AgenteBD.ObtenerAgente.Modificar("UPDATE Personas SET NombrePersona='" & p.nombrePais & "' WHERE IDPersona='" & p.idPais & "';")
+    End Function
 
 End Class
